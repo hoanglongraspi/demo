9afc6866da57ab4386ed2c14e687aec78c40acca
hoanglongraspi <33237818+hoanglongraspi@users.noreply.github.com>
2023-11-05 03:04:49 -0500

    Add files via upload

---
+++ My pose models/main.js
@@ -0,0 +1,85 @@
+// the link to your model provided by Teachable Machine export panel
+    const URL = "./my_model/";
+    let model, webcam, ctx, labelContainer, maxPredictions;
+
+    async function init() {
+        const modelURL = URL + "model.json";
+        const metadataURL = URL + "metadata.json";
+
+        // load the model and metadata
+        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+        // Note: the pose library adds a tmPose object to your window (window.tmPose)
+        model = await tmPose.load(modelURL, metadataURL);
+        maxPredictions = model.getTotalClasses();
+
+        // Convenience function to setup a webcam
+        const size = 200;
+        const flip = true; // whether to flip the webcam
+        webcam = new tmPose.Webcam(size, size, flip); // width, height, flip
+        await webcam.setup(); // request access to the webcam
+        await webcam.play();
+        window.requestAnimationFrame(loop);
+
+        // append/get elements to the DOM
+        const canvas = document.getElementById("canvas");
+        canvas.width = size; canvas.height = size;
+        ctx = canvas.getContext("2d");
+        labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < maxPredictions; i++) { // and class labels
+            labelContainer.appendChild(document.createElement("div"));
+        }
+    }
+
+    async function loop(timestamp) {
+        webcam.update(); // update the webcam frame
+        await predict();
+        window.requestAnimationFrame(loop);
+    }
+
+
+    var x = document.getElementById("audiooo");
+
+    async function predict() {
+        // Prediction #1: run input through posenet
+        // estimatePose can take in an image, video or canvas html element
+        const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+        // Prediction 2: run input through teachable machine classification model
+        const prediction = await model.predictTopK(posenetOutput);
+        // labelContainer.innerHTML = prediction[0].classname;
+        
+        
+        var classPrediction = prediction[0].className ;
+        labelContainer.childNodes[0].innerHTML = classPrediction;
+            var last_posture;
+            if (classPrediction == "Bad posture" && classPrediction !== last_posture){    
+                    last_posture = classPrediction;
+                    console.log("Ngồi Hư quá");
+                    x.play();
+
+            }
+            else if (classPrediction == "Good posture" && classPrediction !== last_posture){    
+                    last_posture = classPrediction;
+                    
+                    console.log("Ngồi giỏi quá");
+            }
+            
+            
+                
+            
+        
+
+        // finally draw the poses
+        drawPose(pose);
+    }
+
+    function drawPose(pose) {
+        if (webcam.canvas) {
+            ctx.drawImage(webcam.canvas, 0, 0);
+            // draw the keypoints and skeleton
+            if (pose) {
+                const minPartConfidence = 0.5;
+                tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+                tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+            }
+        }
+    }
